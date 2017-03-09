7aa60b62c22d9886acedcfa97b8ed95b927f2bea
Keyvan Mosharraf <kayvanmsh@gmail.com>
2017-03-09 16:10:45 -0500

    Added comments and documentation

--- client.py
+++ client.py
@@ -12,31 +12,59 @@ class SDElementsAPIException(Exception):
 
 
 class SDElementsClient(object):
+    business_units_api_path = "/api/v2/business-units/"
+
     def __init__(self, api_token, sde_server="cd.sdelements.com"):
         self.api_token = api_token
         self.server = sde_server
 
     @property
     def authorization_header_dict(self):
+        # In order to authenticate with SDE server the Authorization header needs to be present with API token
         return {'Authorization': "Token " + self.api_token}
 
     @property
     def default_headers(self):
+        """Return a default set of headers (Authorization, Accept, and Content-Type) to be used in API requests"""
         header_dict = {'Accept': "application/json", 'Content-Type': "application/json"}
         header_dict.update(self.authorization_header_dict)
         return header_dict
 
     def build_url(self, path):
+        """Return a url to be used in an API request with path"""
         return "https://" + self.server + path
 
     def create_business_unit(self, name, users=None, groups=None, default_users=None, default_groups=None,
                              all_users=False):
+        """
+        Create a new business unit in the SDElements server.
+
+        Check API docs at https://sdelements.github.io/slate/#create-a-new-business-unit for more info
+
+        :rtype: dict
+        :param name: The name of the new business unit.
+        :param users: A list of users (emails) who are part of the business unit.
+        :param groups: A list of group ids of the groups which are part of the business unit
+        :param default_users: A list of dictionaries representing the default user roles for the users in the business
+            unit. Each dictionary has an email field and a role field where the role is the role id. The users specified
+            here should be members of the business unit unless all_users is true.
+        :param default_groups: A list of dictionaries representing the default group roles for the users in the
+            business unit. Each dictionary has an id field which is the group id and a role field where the role is the
+            role id. The groups specified here should be members of the business unit unless all_users is true.
+        :param all_users: Whether the business unit includes all users. Trying to create a business unit with this field
+            set to True and specific users/groups specified is an error. Default is false.
+        :return: A dictionary representing the newly created business unit
+        """
         request_headers = self.default_headers
-        request_url = self.build_url("/api/v2/business-units/")
+
+        request_url = self.build_url(self.business_units_api_path)
+
         request_body = {'name': name}
         if users:
+            # SDE API needs this field in the format: [{"email": "test@example.com"}, {"email": "test2@example.com"}]
             request_body['users'] = [{'email': user} for user in users]
         if groups:
+            # SDE API needs this field in the format: [{"id": "G1"}, {"id": "G2"}]
             request_body['groups'] = [{'id': group_id} for group_id in groups]
         if default_users:
             request_body['default_users'] = default_users
@@ -47,5 +75,6 @@ class SDElementsClient(object):
 
         response = requests.post(request_url, headers=request_headers, json=request_body)
         if not response.ok:
+            # Request did not go through because of an error, throw an exception with the response to be analyzed later
             raise SDElementsAPIException(response)
         return response.json()
--- example.py
+++ example.py
@@ -2,12 +2,13 @@ import os
 
 from client import SDElementsClient, SDElementsAPIException
 
+# You can set the SDE_API_TOKEN in the environment variables
 API_TOKEN = os.environ['SDE_API_TOKEN']
 
 if __name__ == '__main__':
     client = SDElementsClient(API_TOKEN)
 
     try:
-        print client.create_business_unit("")
+        print client.create_business_unit("Test BU")
     except SDElementsAPIException as e:
         print e.readable_error_message
